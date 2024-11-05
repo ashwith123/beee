7df7e7e47efe49739c20e62ec5b525c2362ca1ca
ashwith <ashwith9492@gmail.com>
2024-11-05 22:26:02 +0530

    completed logic

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const r2 = document.getElementById("r2");
 const slider2 = document.getElementById("r2_slider");
 const r3 = document.getElementById("r3");
 const slider3 = document.getElementById("r3_slider");
+
+const voltage1 = document.getElementById("v1");
+const sliderv1 = document.getElementById("v1_slider");
+
+const voltage2 = document.getElementById("v2");
+const sliderv2 = document.getElementById("v2_slider");
+
 const button = document.querySelector("button");
 const resultDiv = document.getElementById("result");
 
@@ -19,8 +26,22 @@ slider3.addEventListener("input", function () {
   r3.value = slider3.value;
 });
 
+sliderv1.addEventListener("input", function () {
+  v1.value = sliderv1.value;
+});
+
+sliderv2.addEventListener("input", function () {
+  v2.value = sliderv2.value;
+});
+
 button.addEventListener("click", function () {
-  const a = Number(r1.value) + Number(r2.value) + Number(r3.value);
-  console.log(a);
-  resultDiv.textContent = "Total Resistance: " + a;
+  const R1 = Number(r1.value);
+  const R2 = Number(r2.value);
+  const R3 = Number(r3.value);
+  const E1 = Number(voltage1.value);
+  const E2 = Number(voltage2.value);
+
+  // Calculate I3 using the formula
+  const ans = E1 / (R1 + R3) + E2 / (R2 + R3);
+  resultDiv.textContent = "Total current: " + ans;
 });
